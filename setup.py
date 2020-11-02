cba3324cc50a0999eedebcde847e5f289dc0c1ca
Fazus Kazoo <fazuskazoo@gmail.com>
2020-11-02 07:07:31 -0800

    added LICENSE

--- setup.py
+++ setup.py
@@ -3,18 +3,23 @@ from setuptools import setup
 
 here = pathlib.Path(__file__).parent
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
  name="orange",
  version="0.0.1",
  author="Fazus Kazoo",
- descriptiong="Python things",
+ description="Python things",
+ long_description=long_description,
+ long_description_content_type="text/markdown",
  license="GPL",
- url="https://github/fazuskazoo.com/Orangei",
+ url="https://github/fazuskazoo.com/Orange",
  packages=["orange"],
+ python_requires='>=3.8',
  include_package_data=True,
  entry_points={
       "console_scripts": [
-
            ]
      }
 )
