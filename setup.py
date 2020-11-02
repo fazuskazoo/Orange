da4aaba495682a7e4ad58150f2f7c8dab0e90d10
Fazus Kazoo <fazuskazoo@gmail.com>
2020-11-02 12:01:46 -0800

    added distutils.command.build

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 import pathlib
 from setuptools import setup
 from pkg_resources import parse_requirements
+import distutils.command.build as _build
 
 here = pathlib.Path(__file__).parent
 
 with open('requirements.txt') as fp:
     install_reqs = fp.read()
 
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+class SystemBuild(_build.build):
+    def run(self):
+        print("System Build")
 
 setup(
  name="oxrange",
@@ -31,5 +35,7 @@ setup(
  entry_points={
       "console_scripts": [
            ]
-     }
+     },
+ cmdclass={'build': SystemBuild
+     },
 )
