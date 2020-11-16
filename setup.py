dc6143952b71958021310d7d21b63f4e76e91e17
Fazus Kazoo <fazuskazoo@gmail.com>
2020-11-16 10:02:31 -0800

    installed start.json

--- setup.py
+++ setup.py
@@ -24,43 +24,6 @@ with open("README.md", "r") as fh:
 '''
 
 
-CUSTOM_COMMANDS = [['echo', 'Custom command worked!'],
-                   ['sudo', 'apt-get', 'install', '--simulate', 'vim'],
-                   ['pip', 'install', 'oxrange'],
-                  ]
-
-class build(_build):
-   print("**      build()")
-   sub_commands = _build.sub_commands + [('CustomCommands', None)]
-
-class CustomCommands(Command):
-      def initialize_options(self):
-              pass
-
-      def finalize_options(self):
-              pass
-
-      def RunCustomCommand(self, command_list):
-           print('Running command: %s' % command_list)
-
-      def run(self):
-           print("   run         ")
-           for command in CUSTOM_COMMANDS:
-                self.RunCustomCommand(command)
-
-
-class SystemBuild(_build):
-    def run(self):
-        print("********************** System Build")
-        print(sysinstall_reqs)
-
-
-class CustomInstall(install):
-    def run(self):
-        print("********************** Custom Install")
-
-        sub_commands = _build.sub_commands + [('CustomCommands', None)]
-        install.run(self)
 
 """
 https://github.com/apache/beam/blob/master/sdks/python/apache_beam/examples/complete/juliaset/setup.py
@@ -71,7 +34,7 @@ https://www.anomaly.net.au/blog/running-pre-and-post-install-jobs-for-your-pytho
 
 """
 
-data_files = [ ('start', ['start.json'] ) ]
+
 
 setup(
  name="oxrange",
@@ -85,15 +48,13 @@ setup(
  url="https://github.com/fazuskazoo/Orange",
  python_requires='>=3.6',
  include_package_data=True,
- data_files=data_files,
- packages=setuptools.find_packages(),
+ packages=['orange'],
+ package_data={
+  'orange':['*.json'],
+
+     },
  classifiers=[
   "Programming Language :: Python :: 3",
   "Operating System :: POSIX :: Linux",
  ],
-         cmdclass={
-                   'CustomCommands': CustomCommands,
-                   'install' : CustomInstall,
-                   'build' : build
-     },
 )
