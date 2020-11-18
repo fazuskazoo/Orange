d5eecf1642d3a6110828d839a50d64f89d354d80
Fazus Kazoo <fazuskazoo@gmail.com>
2020-11-18 08:58:41 -0800

    added start() method to init

--- orange/__init__.py
+++ orange/__init__.py
@@ -1,2 +1,7 @@
-#from orange import orange
-#print("init orange")
+from orange import config 
+
+def start():
+  if not config.get_installed():
+      print("installing ...")
+      config.set_installed(True)
+  print(config.get_installed())
